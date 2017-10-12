e1e775a2a9b5dcefeb01897728881ddfcf9c0384
almaat <karibayevalmat@gmail.com>
2017-10-13 01:21:12 +0600

    login back end

--- src/Login.vue
+++ src/Login.vue
@@ -1,30 +1,55 @@
 <template>
 <div>
     <div id="login">
-        <form novalidate @submit.stop.prevent="submit">
+        <form novalidate @submit.stop.prevent="login">
             <md-input-container>
                 <label>Login</label>
-                <md-input required placeholder="Login"></md-input>
+                <md-input required placeholder="Login" v-model="username"></md-input>
             </md-input-container>
             <md-input-container md-has-password>
                 <label>Password</label>
-                <md-input required type="password" placeholder="Password"></md-input>
+                <md-input required type="password" placeholder="Password" v-model="password"></md-input>
             </md-input-container>
+            <md-button class="md-primary" type="submit">GO</md-button>
         </form>
     </div>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+
     data() {
         return {
-
+            username: "",
+            password: "",
+            loginEndPoint: "http://localhost:8080/oauth/token"
         }
     },
 
     methods: {
-
+        login: function(){
+            function getCookie(name) {
+              var value = "; " + document.cookie;
+              var parts = value.split("; " + name + "=");
+              if (parts.length == 2) return parts.pop().split(";").shift();
+            }
+            var params = new URLSearchParams();
+            params.append('grant_type', 'password');
+            params.append('username', this.username);
+            params.append('password', this.password);
+            axios({
+                method:'post',
+                url:this.loginEndPoint,
+                auth:{username:'my-trusted-client',password:'secret'},
+                headers: {"Content-type": "application/x-www-form-urlencoded; charset=utf-8"},
+                data:params
+            }).then(function(response){
+                document.cookie = "access_token =" + response.data.access_token + ";path=/";
+                console.log(getCookie('access_token'));
+            });
+        }
     }
 
 
@@ -34,5 +59,9 @@ export default {
     #login{
         width:30%;
         margin:170px auto 0 auto;
+    }
+    .md-button{
+        width: 100%;
+        font-size:19px;
     }
 </style>
